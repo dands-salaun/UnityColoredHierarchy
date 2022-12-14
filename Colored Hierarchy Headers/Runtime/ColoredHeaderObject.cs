6c3d0b32e04ebd252b7f3eec02248c84c72de535
Dandara Salaun <43423357+dands-salaun@users.noreply.github.com>
2022-12-14 16:32:51 -0300

    first version

---
+++ Colored Hierarchy Headers/Runtime/ColoredHeaderObject.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ColoredHeaderObject : MonoBehaviour
+{
+    [Header("Preset")] 
+    [SerializeField] private ColoredHeaderPreset preset;
+
+    [SerializeField] private ColoredHeaderSettings settings;
+
+    public ColoredHeaderSettings GetSettings()
+    {
+        return settings;
+    }
+
+    private void SetPresetSettings()
+    {
+        settings = preset.GetHeaderPreset().settings;
+    }
+
+    public string GetName()
+    {
+        if (preset == null)
+        {
+            return gameObject.name;
+        }
+        return preset.GetHeaderPreset().namePreset;
+    }
+    
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        EditorApplication.RepaintAnimationWindow();
+
+        if (preset != null)
+        {
+            SetPresetSettings();
+        }
+    }
+    
+#endif
+}
